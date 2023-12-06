eb78d4e7877b772ef56b081d1159cdb1a375a924
Sean Stocker <sean.stocker14@gmail.com>
2023-12-06 13:31:07 +0000

    feat: installed prisma adapter for nex-auth feat: updated todo list feat: updated prisma schema file with next-auth schemas feat: updated next auth routes to use nodejs as runtime due to edge causing OAuthCallback errors feat: updated next-auth configuration with prisma adapter to use and view next-auth schemas

--- src/auth.ts
+++ src/auth.ts
@@ -1,6 +1,8 @@
 import NextAuth from "next-auth";
 import type { NextAuthConfig } from "next-auth";
 import GitHub from "next-auth/providers/github";
+import { PrismaAdapter } from "@auth/prisma-adapter";
+import prisma from "@/lib/prisma";
 
 export const {
   handlers: { GET, POST },
@@ -8,5 +10,7 @@ export const {
   signIn,
   signOut,
 } = NextAuth({
+  debug: process.env.NODE_ENV === "development",
+  adapter: PrismaAdapter(prisma),
   providers: [GitHub],
 } satisfies NextAuthConfig);
